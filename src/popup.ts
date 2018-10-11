32ad09e6da85d5a12bdbd234d0a9d71ccae95b0e
Łukasz <hellanix.bialek@gmail.com>
2018-10-11 18:33:06 +0200

    prettier popup js

--- src/popup.ts
+++ src/popup.ts
@@ -1,67 +1,51 @@
-
-var port: chrome.runtime.Port;
-var startedScript = false;
-
-
-function enableScript() {
-	
-	document.getElementById("toggleBtnScript").classList.add("btn-warning");
-	document.getElementById("toggleBtnScript").classList.remove("btn-success");
-	document.getElementById("toggleBtnScript").innerHTML = "Disable bot";
-	port.postMessage("EnableScript");
-	
-}
-
-function disableScript() {
-	
-	document.getElementById("toggleBtnScript").classList.add("btn-success");
-	document.getElementById("toggleBtnScript").classList.remove("btn-warning");
-	document.getElementById("toggleBtnScript").innerHTML = "Enable bot";
-	port.postMessage("DisableScript");
-	
-}
-
-function getSavedInfo(msg: string) {
-	
-	// var index = msg.indexOf(" ");
-	// var id = msg.substr(0, index);
-	// var text = msg.substr(index + 1);
-		
-	if(msg == "ScriptEnabled") {
-		startedScript = true;
-		document.getElementById("toggleBtnScript").classList.add("btn-warning");
-		document.getElementById("toggleBtnScript").classList.remove("btn-success");
-		document.getElementById("toggleBtnScript").innerHTML = "Disable bot";
-	}
-	
-	if(msg == "ScriptDisabled")
-		startedScript = false;
-}
-
-function toggleScript() {
-	
-	if(startedScript)
-		disableScript();
-	else
-		enableScript();
-	
-	startedScript = !startedScript;
-	
-}
-
+import * as values from "./values.js";
+let port: chrome.runtime.Port;
+let startedScript = false;
 
 document.addEventListener('DOMContentLoaded', () => {	
 	
 	port = chrome.runtime.connect({
 		name: "Sample Communication"
 	});
+
+	readScriptStateFromBackground();
 	
-	port.onMessage.addListener(function(msg) {
-		getSavedInfo(msg);
-	});
-	
-	var toggleBtnScript = document.getElementById('toggleBtnScript');
+	const toggleBtnScript = document.getElementById('toggleBtnScript');
 	toggleBtnScript.addEventListener("click", toggleScript , false );
   
 });
 
+function readScriptStateFromBackground() {
+	port.onMessage.addListener(function(msg) {
+		if(msg === values.MSG_SCRIPT_ENABLED) {
+			enableScript()
+		}
+		if(msg === values.MSG_SCRIPT_DISABLED)
+			disableScript()
+	});
+}
+
+function toggleScript() {
+	if(startedScript) {
+		disableScript();
+		port.postMessage(values.MSG_SCRIPT_DISABLED);
+	}
+	else {
+		enableScript();
+		port.postMessage(values.MSG_SCRIPT_ENABLED);
+	}
+}
+
+function enableScript() {
+	document.getElementById("toggleBtnScript").classList.add("btn-warning");
+	document.getElementById("toggleBtnScript").classList.remove("btn-success");
+	document.getElementById("toggleBtnScript").innerHTML = "Disable bot";
+	startedScript = true;
+}
+
+function disableScript() {
+	document.getElementById("toggleBtnScript").classList.add("btn-success");
+	document.getElementById("toggleBtnScript").classList.remove("btn-warning");
+	document.getElementById("toggleBtnScript").innerHTML = "Enable bot";
+	startedScript = false;
+}
